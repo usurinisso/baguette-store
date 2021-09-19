69665ab547a38b029a892887d763222716f75ca7
usurinisso <tautvis477@gmail.com>
2021-09-19 14:51:00 +0300

    feat: configs for heroku

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(process.env.PORT);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
