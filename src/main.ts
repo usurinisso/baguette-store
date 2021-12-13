0cb674abaeb9d02a486cc21e550c240cbdf3ab03
Tautvydas Dževečka <tautvydas.dzevecka@telesoftas.com>
2021-12-13 11:16:26 +0200

    no cors

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors({
-    origin: 'https://baguette-store-web.herokuapp.com',
-    credentials: true,
-  });
   await app.listen(process.env.PORT || 3000);
 }
 
