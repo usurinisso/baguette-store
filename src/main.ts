a3c7ae104da864622cad7961cbc6ef475a9c8c7d
Tautvydas Dževečka <tautvydas.dzevecka@telesoftas.com>
2021-12-13 11:44:29 +0200

    a

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.enableCors({ origin: '*', allowedHeaders: '*' });
   await app.listen(process.env.PORT || 3000);
 }
 
